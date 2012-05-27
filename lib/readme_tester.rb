0c855e9386bf07575fbb16b4d2329455eee59daa
Josh Cheek <josh.cheek@gmail.com>
2012-05-27 05:19:15 -0500

    Extract some methods

--- lib/readme_tester.rb
+++ lib/readme_tester.rb
@@ -14,27 +14,45 @@ class ReadmeTester
   end
 
   def execute
-    filename = argv.first
-    if !filename
-      stderr.write 'Please provide an input file'
-      1
-    elsif filename !~ /\.testable_readme$/
-      stderr.write "#{filename.inspect} does not end in .testable_readme"
-      1
-    elsif !file_class.exist?(filename)
-      stderr.write "#{filename.inspect} does not exist"
-      1
-    else
-      body = file_class.read_file filename
-      file_class.write_file output_filename_for(filename), body
-      0
-    end
+    missing_input_file || invalid_filename || nonexistent_file || execute!
   end
 
 private
 
+  def missing_input_file
+    return if filename
+    stderr.write 'Please provide an input file'
+    1
+  end
+
+  def invalid_filename
+    return if filename =~ suffix_regex
+    stderr.write "#{filename.inspect} does not end in .testable_readme"
+    1
+  end
+
+  def nonexistent_file
+    return if file_class.exist? filename
+    stderr.write "#{filename.inspect} does not exist"
+    1
+  end
+
+  def execute!
+    body = file_class.read_file filename
+    file_class.write_file output_filename_for(filename), body
+    0
+  end
+
+  def filename
+    argv.first
+  end
+
   def output_filename_for(filename)
-    filename.sub /\.testable_readme/, ''
+    filename.sub suffix_regex, ''
+  end
+
+  def suffix_regex
+    /\.testable_readme$/
   end
 
   attr_accessor :argv
