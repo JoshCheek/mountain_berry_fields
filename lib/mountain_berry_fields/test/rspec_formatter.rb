35b2706bd23e1bfe50aec7f4974bb45d10737054
Josh Cheek <josh.cheek@gmail.com>
2012-07-01 15:26:46 -0500

    Move require statement to top of file

--- lib/mountain_berry_fields/test/rspec_formatter.rb
+++ lib/mountain_berry_fields/test/rspec_formatter.rb
@@ -1,11 +1,11 @@
 require 'rspec/core/formatters/base_formatter'
+require 'json'
 
 class MountainBerryFields
   class Test
     class RSpec
       class Formatter < ::RSpec::Core::Formatters::BaseFormatter
         def example_failed(example)
-          require 'json'
           print JSON.dump 'full_description' => example.full_description,
                           'message'          => example.exception.message,
                           'backtrace'        => format_backtrace(example.exception.backtrace, example)
